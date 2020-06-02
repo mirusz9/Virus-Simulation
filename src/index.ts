bb60c3f6169497e82a9a96f52fec1f6db04ec56b
mirusz9 <adam.sztano@gmail.com>
2020-06-02 16:33:42 +0200

    not using p5 js, created canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+const canvas = document.querySelector("canvas")!;
+const c = canvas.getContext("2d")!;
+canvas.width = 1000;
+canvas.height = 500;
+
+c.fillStyle = "#ffffff";
+c.fillRect(0, 0, canvas.width, canvas.height);
